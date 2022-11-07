3ffa7abf334614c3bd5f8b216513e627ab9480a2
JenifferBarrios <jbarrios03@hotmail.com>
2022-11-07 17:57:11 -0500

    Evaluando si la ruta es relativa o absoluta

--- funciones.js
+++ funciones.js
@@ -1,9 +1,28 @@
 //Leer una ruta 
 const fs = require('fs');
 const path = require("path");
+const resolve = require('resolve')
 
-// const ruta = process.argv[2];
-// console.log(path.isAbsolute(ruta));
+const ruta = process.argv[2]; //módulo que permite capturar argumentos a través de la línea de comandos y se guarda como un array.
+// console.log(path.isAbsolute(ruta)); //para evaluar cuando la ruta es absoluta
+const evRuta = (path.isAbsolute(ruta));
+const ingRuta = (path,ruta) => {
+  if (ruta == " ") {
+    alert("La ruta no existe")
+  } else if(ruta == verdadero ) {
+    //entrar al directorio
+    
+  }
+  else {
+    (ruta == falso)
+    path.resolve(ruta)
+  }
+}
+console.log (ingRuta)
+
+
+
+
 
 fs.readFile(
   "C:/Users/LABORATORIA/Desktop/Proyectos/Proyecto 4/BOG005-md-links/funciones.js",
